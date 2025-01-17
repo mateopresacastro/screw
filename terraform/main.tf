19c1cf0928233d3a3f32343094d555eba8d86a31
Mateo Presa Castro <mateopresacastro@gmail.com>
2025-01-17 21:58:17 +0100

    chore: id

--- terraform/main.tf
+++ terraform/main.tf
@@ -186,6 +186,13 @@ resource "aws_iam_role_policy" "github_actions" {
           "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:document/AWS-RunShellScript",
           "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:instance/${aws_instance.screw_server.id}"
         ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:SendCommand"
+        ]
+        Resource = "arn:aws:ssm:${data.aws_region.current.name}::document/AWS-RunShellScript"
       }
     ]
   })
